42b6aa4a41beb2857df27210dc0f82c5ce4b3d6a
Giadagenovesi <giada.g@outlook.it>
2023-09-07 15:25:52 +0200

    Add project files.

---
+++ csharp-functions/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace csharp_functions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+
+
+
+            //FUNCTIONS
+
+            //void StampaArray(int[] array): che preso un array di numeri interi, stampa a video il contenuto dell’array in questa forma “[elemento 1, elemento 2, elemento 3, ...]”.
+            void PrintArray(int[] array)
+            {
+                Console.Write("[");
+
+                for (int i = 0; i < array.Length; i++)
+                {
+                    if (i < array.Length - 1)
+                    {
+                        Console.Write(array[i] + ", ");
+                    }
+                    else
+                    {
+                        Console.Write(array[i]);
+                    }
+                }
+
+                Console.WriteLine("]");
+            }
+
+        }
+    }
+}
